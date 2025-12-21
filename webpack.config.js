e5af5c6666b8d36e6e4068951516086a0ede638d
Jay <snootched@gmail.com>
2025-12-21 06:54:42 -0500

    refactor: update dependencies and remove Monaco Editor integration
    - Removed Monaco Editor and Monaco YAML from devDependencies.
    - Added CodeMirror packages for enhanced code editing features.
    - Cleaned up webpack configuration by removing Monaco-specific loaders and chunking.

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,7 @@ module.exports = {
     output: {
         filename: 'lcards.js',
         path: path.resolve(__dirname, 'dist'),
-        // Public path for dynamic imports - will be auto-detected at runtime
         publicPath: 'auto',
-        // Enable code splitting for async chunks
         chunkFilename: '[name].[contenthash:8].js',
     },
     module: {
@@ -15,19 +13,6 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-            },
-            // Add loader for Monaco Editor CSS
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            // Add loader for Monaco Editor fonts
-            {
-                test: /\.ttf$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'fonts/[name].[hash:8][ext]'
-                }
             },
         ],
     },
@@ -39,21 +24,7 @@ module.exports = {
         splitChunks: {
             chunks: 'async',
             cacheGroups: {
-                // Monaco Editor core
-                monaco: {
-                    test: /[\\/]node_modules[\\/]monaco-editor[\\/]/,
-                    name: 'monaco-editor',
-                    priority: 20,
-                    reuseExistingChunk: true,
-                },
-                // Monaco YAML worker
-                monacoYaml: {
-                    test: /[\\/]node_modules[\\/]monaco-yaml[\\/]/,
-                    name: 'monaco-yaml',
-                    priority: 20,
-                    reuseExistingChunk: true,
-                },
-                // Default vendor chunk for other large libraries
+                // Default vendor chunk for large libraries
                 defaultVendors: {
                     test: /[\\/]node_modules[\\/]/,
                     priority: -10,
